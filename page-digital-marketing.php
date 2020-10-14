c722e5e14b8b8bb807ed24652df1ce713055faad
Reza Khoshghalb <reza.toosi@gmail.com>
2020-10-14 23:56:44 +0330

    services single page design

--- page-digital-marketing.php
+++ page-digital-marketing.php
@@ -14,14 +14,14 @@
   itcorp_services_heading_section( [
     'link'      =>  '/services',
     'subtitle'  =>  'Top Rated Digital Services',
-    'title'     =>  'Professional Web Design Services',
-    'text'      =>  'At RIOWEX we combine professional skills, art, powerful tools and latest technologies to create incredible marketing solutions.',
-    'img'       =>  get_template_directory_uri() . '/img/iconic/webdesign-4.png',
-    'img-alt'   =>  'web design satisfaction guarantee',
+    'title'     =>  'Professional Digital Marketing Services',
+    'text'      =>  '...',
+    'img'       =>  get_template_directory_uri() . '/img/iconic/digital-marketing.png',
+    'img-alt'   =>  'digital marketing satisfaction guarantee',
     ] );
 ?>
 <?php require get_template_directory() . '/page-templates/services-guarantee.php'; ?>
-<?php require get_template_directory() . '/page-templates/techs.php'; ?>
+
 
 <?php
 get_footer();
